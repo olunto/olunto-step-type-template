3d0f215eb843f5090b3ac6739cff717b6e74a76d
Fortune Ngwenya <fortunengwenya@gmail.com>
2023-12-28 16:10:24 +0200

    update

---
+++ logic/Models/Settings.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace StepType.Models;
+
+internal class Settings
+{
+	[JsonRequired]
+	public string Setting1 { get; set; }
+}
---
+++ logic/Step.cs
@@ -0,0 +1,49 @@
+﻿using AppEngine.Common;
+using Microsoft.Extensions.Logging;
+using StepType.Models;
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace StepType;
+
+public sealed class Step : IStep
+{
+	private readonly Settings _settings;
+	private readonly IStepContext _context;
+	
+	public Step(IStepContext context)
+	{
+		ArgumentNullException.ThrowIfNull(context);
+
+		_context = context;
+		_settings = JsonSerializer.Deserialize<Settings>(context.StepInfo.Settings);
+	}
+
+	public async Task InitializeAsync()
+	{
+		await Task.CompletedTask;
+	}
+
+	public async Task RunAsync()
+	{
+		await Task.CompletedTask;
+	}
+
+	public async Task InputAsync(string inboxId, IRow row)
+	{
+		// resolve
+
+		var setting1 = _context.ValueExpressionHelper.Resolve<string>(row, _settings.Setting1);
+		
+		// process
+
+		row.Column("Name2", ColumnType.String, setting1);
+
+		_context.Logger.LogInformation("Setting1 : {Value}", setting1);
+
+		// output
+
+		await _context.OutputAsync(row, "Outbox1");
+	}
+}
